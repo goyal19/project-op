229b59ac541a87966555e42c45820c4a1761add3
goyal19 <101124248+goyal19@users.noreply.github.com>
2022-12-28 13:18:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/OResxS_LL/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    
+    }
+    else{
+        console.log(results);
+        random_number_r=Math.floor(Math.random() * 255) +1;
+        random_number_g=Math.floor(Math.random() * 255) +1;
+        random_number_b=Math.floor(Math.random() * 255) +1;
+        
+       document.getElementById("result_label").innerHTML= 'i can hear - '+results[0].label;
+       document.getElementById("result_confidence").innerHTML='accuracy - '+(results[0].confidence*100).toFixed(2)+" %";
+       document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+       document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+      
+       img=document.getElementById("dog");
+       img1=document.getElementById("cat");
+    
+
+       if(results[0].label=="barking"){
+        img.src="dog.gif";
+        img1.src="cat.png";
+
+        
+       }
+
+ else if(results[0].label=="meow"){
+        img.src="dog.png";
+        img1.src="cat.gif";
+        
+        
+       }
+     else if(results[0].label=="roar"){
+        img.src="dog.gif";
+        img1.src="cat.png";
+        
+        
+       }
+       else if{result[0].label=="mooing"
+        img.src="dog.png";
+        img1.src="cat.gif";
+        
+        
+       }
+       
+       
+    }
+    }
